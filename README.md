6e1f939f24da87694c6a987bcb7c7119ce8d227a
dxsfung <davxsfung@gmail.com>
2017-03-23 18:16:58 +0800

    tidy up and added README.md info

--- README.md
+++ README.md
@@ -1,1 +1,15 @@
 # ruby-rock_paper_scissors
+
+## Rock, Paper, Scissors
+This is a recreation of the game Rock, Paper, Scissors in a website. This can be a two player game, so there would be two 'plays' in each round.
+
+A method was written to handle the different combinations of plays.
+
+Specs for all possible combination of plays were written. After all specs are passing, the website was built where someone can go to play.
+
+## Bonus points: Found a way to get the computer to randomly select a 'play' and so the website can entertain a one player game.
+
+## Note:
+
+* This website is mobile responsive using Bootstrap
+* This website is deployed on Heroku: https://guarded-meadow-73270.herokuapp.com/
--- lib/rock_paper_scissors.rb
+++ lib/rock_paper_scissors.rb
@@ -3,31 +3,31 @@ require('pry')
 class String
     define_method(:beats) do |argument|
         if argument == "computer"
-          int=rand(3)
-          weapons = {0=>"rock", 1=>"paper", 2=>"scissors"}
-          argument = weapons.fetch(int)
+            int=rand(3)
+            weapons = {0=>"rock", 1=>"paper", 2=>"scissors"}
+            argument = weapons.fetch(int)
         end
         player1 = self
         player2 = argument
         result = {}
         if player1 == "rock"
             if player2 == "rock"
-              result[:display]= "Ties with"
-              result[:boolean]= false
-              result[:player2]= "rock"
+                result[:display]= "Ties with"
+                result[:boolean]= false
+                result[:player2]= "rock"
                 return result
             elsif player2 == "paper"
                 # return "Loses to"
                 result[:display]= "Loses to"
                 result[:boolean]= false
                 result[:player2]= "paper"
-                  return result
+                return result
             elsif player2 == "scissors"
                 # return "Wins"
                 result[:display]= "Wins"
                 result[:boolean]= true
                 result[:player2]= "scissors"
-                  return result
+                return result
             end
         elsif player1 == "paper"
             if player2 == "rock"
@@ -35,19 +35,19 @@ class String
                 result[:display]= "Wins"
                 result[:boolean]= true
                 result[:player2]= "rock"
-                  return result
+                return result
             elsif player2 == "paper"
                 # return "Ties with"
                 result[:display]= "Ties with"
                 result[:boolean]= false
                 result[:player2]= "paper"
-                  return result
+                return result
             elsif player2 == "scissors"
                 # return "Loses to"
                 result[:display]= "Loses to"
                 result[:boolean]= false
                 result[:player2]= "scissors"
-                  return result
+                return result
             end
         elsif player1 == "scissors"
             if player2 == "rock"
@@ -55,19 +55,19 @@ class String
                 result[:display]= "Loses to"
                 result[:boolean]= false
                 result[:player2]= "rock"
-                  return result
+                return result
             elsif player2 == "paper"
                 # return "Wins"
                 result[:display]= "Wins"
                 result[:boolean]= true
                 result[:player2]= "paper"
-                  return result
+                return result
             elsif player2 == "scissors"
                 # return "Ties with"
                 result[:display]= "Ties with"
                 result[:boolean]= false
                 result[:player2]= "scissors"
-                  return result
+                return result
             end
         end
     end
